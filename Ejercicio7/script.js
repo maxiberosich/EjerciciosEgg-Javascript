784633e731e6350c2d10be7a95c10c63fd4139ae
maxiberosich <mberosich@gmail.com>
2023-08-19 19:53:50 -0300

    Correccion Ej7

--- Ejercicio7/script.js
+++ Ejercicio7/script.js
@@ -2,12 +2,36 @@
 solicite números al usuario hasta que la suma de los números introducidos supere el
 límite inicial.*/
 
-let limite = parseInt(document.getElementById("ingresoUsuario").value);
-var suma = 0;
+const inputL = document.getElementById("ingresoUsuario");
+const boton = document.getElementById("btn");
+const texto = document.getElementById("resultadoSuma");
 
-document.getElementById("btn").onclick = () => {
-    document.getElementById("ingresoUsuario").value = 0;
-    document.getElementById("mensaje").textContent = "Ingrese un numero";
-    suma = parseInt(document.getElementById("ingresoUsuario").value) + suma;
-    document.getElementById("resultadoSuma").textContent = `la suma de los numeros ingresados hasta el momento es : ${suma}`;
-}
+let limite = 0;
+let suma = 0;
+let contador = 0;
+
+boton.addEventListener("click", function(){
+    const valor = parseInt(inputL.value);
+
+    if(isNaN(valor) || valor <= 0){
+        alert("Ingresa un valor valido");
+        return;
+    }
+
+    if(limite === 0){
+        limite = valor;
+        texto.textContent = `Ingresa numero hasta llegar el limite ${limite}`;
+        document.getElementById("mensaje").textContent = "Ingrese un numero";
+    }else{
+        suma += valor;
+        contador++;
+        texto.textContent = `La suma hasta el momento es: ${suma}`;
+
+        if(suma >= limite){
+            texto.textContent = `Se introdujeron ${contador} numeros para alcanzar o superar el limite`;
+            boton.disabled = true;
+        }
+    }
+
+    inputL.value = "";
+})
